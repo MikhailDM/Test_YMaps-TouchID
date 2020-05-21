4fe11869a659e26b5d97d8d6e74dab1443f80dc2
Mike Dmitriev <misha.dmitriev.spb@gmail.com>
2020-05-21 13:49:52 +0300

    CoreLocation Implement

--- WAI/ViewController.swift
+++ WAI/ViewController.swift
@@ -5,14 +5,18 @@
 //  Created by Михаил Дмитриев on 21.05.2020.
 //  Copyright © 2020 Mikhail Dmitriev. All rights reserved.
 //
-
+import CoreLocation
 import UIKit
 
 class ViewController: UIViewController {
+    //Manager
+     let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager.requestWhenInUseAuthorization()
+        //locationManager.requestLocation()
     }
 
 
