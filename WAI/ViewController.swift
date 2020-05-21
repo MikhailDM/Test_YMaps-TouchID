10321fb594ff0b9dc99ae790f066ff32e4e709be
Mike Dmitriev <misha.dmitriev.spb@gmail.com>
2020-05-21 12:57:12 +0300

    Initial

---
+++ WAI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WAI
+//
+//  Created by Михаил Дмитриев on 21.05.2020.
+//  Copyright © 2020 Mikhail Dmitriev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
